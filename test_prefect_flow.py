3678012b8dec521b973cd8c4b571358ff5a9f6a2
Ryan <ryanhomrh@gmail.com>
2024-04-28 16:14:04 -0700

    prefect flow examples

---
+++ test_prefect_flow.py
@@ -0,0 +1,16 @@
+from prefect_dbt.cli.commands import DbtCoreOperation
+from prefect import flow
+
+@flow
+def trigger_dbt_flow() -> str:
+    result = DbtCoreOperation(
+        commands=["dbt build -t dev_ryan"],
+        project_dir="C:/Users/ryanh/GITHUB_FOLDERS/jaffle_shop/dbt-tutorial",
+        profiles_dir="~/.dbt"
+    ).run()
+    return result
+
+if __name__ == "__main__":
+    trigger_dbt_flow.serve(name="testing-deployment",
+                      tags=["testing"],
+                      interval=60)
