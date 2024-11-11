aab66b1861b873586c86838b9b0fe7fdf204f614
Ryan <ryanhomrh@gmail.com>
2024-11-10 19:54:39 -0800

    testing prefect flow

--- test_prefect_flow.py
+++ test_prefect_flow.py
@@ -8,8 +8,10 @@ def trigger_dbt_flow() -> str:
         project_dir="C:/Users/ryanh/GITHUB_FOLDERS/jaffle_shop/dbt-tutorial",
         profiles_dir="~/.dbt"
     ).run()
+    print(result)  # Add this line to inspect the result
     return result
 
 if __name__ == "__main__":
+    ##trigger_dbt_flow.test()
     trigger_dbt_flow.serve(name="testing-deployment",
-                      tags=["testing"])
+                     tags=["testing"])
